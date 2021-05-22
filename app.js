33d35691525f13ce7a3338010fa14852dd8f2fa6
newspaperhobo <hoboinabox@gmail.com>
2021-05-22 12:25:02 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const path = require('path');
+const routes = require('./routes/index');
+require('./config/connection');
+const app = express();
+// if process.env.port doesn't exist, use port 3000
+const PORT = process.env.port || 3000;
+
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
